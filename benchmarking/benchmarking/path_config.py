948e71f7e3e97119adb6c662e4fbc84432a026c5
mzouink <zouinkhi.marwan@gmail.com>
2022-04-17 21:23:27 -0400

    nrs test

--- benchmarking/benchmarking/path_config.py
+++ benchmarking/benchmarking/path_config.py
@@ -1,2 +1,2 @@
-benchmark_path = "/Users/Marwan/Desktop/activelearning/benchmarks/best_commit"
+benchmark_path = "/Users/Marwan/Desktop/activelearning/benchmarks/"
 data_path = "/Users/Marwan/Desktop/activelearning/data/versioned_data"
